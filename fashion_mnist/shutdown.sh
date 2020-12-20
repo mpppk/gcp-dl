10a6b437618d52acc419a5135c78f57978d5173d
mpppk <niboshiporipori@gmail.com>
2020-12-20 17:11:46 +0900

    Update shutdown.sh

--- fashion_mnist/shutdown.sh
+++ fashion_mnist/shutdown.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
-curl -X POST --data-urlencode "payload={\"channel\": \"#notification\", \"username\": \"webhookbot\", \"text\": \"dl instance will be shutdown. \", \"icon_emoji\": \":ghost:\"}" https://hooks.slack.com/services/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
+export SLACK_URL=''
+
+curl -X POST --data-urlencode "payload={\"channel\": \"#notification\", \"username\": \"webhookbot\", \"text\": \"dl instance will be shutdown. \", \"icon_emoji\": \":ghost:\"}" $SLACK_URL
